8c78b17c11e1467fd129a0dc88a559f0b3761df1
Femi <47614924+femid@users.noreply.github.com>
2022-04-02 10:45:15 -0700

    Update app.py
    Adding getdate function for testing

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from datetime import datetime
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
+
+# Get Today's Date in ISO 8601 format
+def get_date():
+    my_date = datetime.now()
+    today = my_date.strftime('%Y-%m-%d')
+    data = {"Today": today}
+
+    return json.dumps(data)
+
+'''
 def hello():
-    return "Hello, World!"
+    #return "Hello, World!"
+
+    # call Get Date function
+    todays_date = get_date()
+    return todays_date
+'''
+
+
